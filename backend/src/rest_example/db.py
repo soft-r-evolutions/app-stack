bdd492914081333b795a04dfe241249d3463ed4c
Bousquet <bousquet.laurent12@gmail.com>
2021-05-17 18:02:45 +0200

    [FEAT] First test

--- backend/src/rest_example/db.py
+++ backend/src/rest_example/db.py
@@ -2,12 +2,30 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
 class Db:
-    def __init__(self):
-        client = MongoClient("mongodb://mongo")
+    ARG_SERVER_URI_KEY = "server_uri"
+    DEFAULT_SERVER_URI = "mongodb://mongo"
+
+    def __init__(self, **kwargs):
+        self.server = kwargs.get(Db.ARG_SERVER_URI_KEY, Db.DEFAULT_SERVER_URI)
+
+        self.client = MongoClient(self.server)
+
+    def connect(self):
         try:
             # The ismaster command is cheap and does not require auth.
-            client.admin.command('ismaster')
-            dbs = client.list_database_names()
-            print("list databases: {}".format(dbs))
+            self.client.admin.command('ismaster')
+            return True
         except ConnectionFailure:
             print("Server not available")
+            return False
+
+    def create_db(self, database_name):
+        self.client[database_name]
+
+    def drop_db(self, database_name):
+        self.client.drop_database(database_name)
+
+    def get_database_names():
+        dbs = self.client.list_database_names()
+        print("list databases: {}".format(dbs))
+        return dbs
--- backend/src/tests/test_db.py
+++ backend/src/tests/test_db.py
@@ -1,3 +1,9 @@
+from rest_example.db import Db
+
+def test_connect():
+    db = Db()
+    assert db.connect()
 
 def test_toto():
     assert True
+
