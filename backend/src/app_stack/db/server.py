f3d8f0595926acddb6c7d6075dc2d4d3f34081cd
Bousquet <bousquet.laurent12@gmail.com>
2021-05-23 00:32:24 +0200

    [FEAT] Add type method

--- backend/src/app_stack/db/server.py
+++ backend/src/app_stack/db/server.py
@@ -13,6 +13,21 @@ class Server:
 
     DEFAULT_CONNECT_TIMEOUT = 2000
 
+    ### Types
+    TYPES_COLLECTION_NAME = "types"
+
+    TYPE_NAME_KEY = "name"
+
+    TYPE_VERSION_KEY = "version"
+    DEFAULT_TYPE_VERSION = 1
+
+    TYPE_COLLECTION_KEY = "collection"
+    TYPE_PRIMITIVE_VALUE = "primitive"
+    DEFAULT_TYPE_COLLECTION = TYPE_PRIMITIVE_VALUE
+
+    TYPE_STRING_VALUE = "String"
+    TYPE_NUMBER_VALUE = "Number"
+
     def __init__(self, **kwargs):
         print("Declare MongoDb Server:")
         self.server_uri = kwargs.get(Server.ARG_SERVER_URI_KEY, Server.DEFAULT_SERVER_URI)
@@ -41,8 +56,10 @@ class Server:
             print("***** MongoDb Server is not available. *****")
             return False
 
+
     def is_connected(self):
         return self._is_connected
+
 
     def select_db(self, **kwargs):
         if not self.is_connected:
@@ -61,6 +78,16 @@ class Server:
             print("Create option detected for unexisting MongoDb Server database: {}".format(self.database_name))
             return self.create_db()
 
+        print("Unexisting MongoDb Server database: {} can not be selected".format(self.database_name))
+        return False
+
+
+    def has_database_selected(self):
+        if not self.is_connected:
+            return False
+        return True if self.database else False
+
+
     def exists_db(self, **kwargs):
         if not self.is_connected:
             print("Server must be connected to check if a database exists.")
@@ -69,6 +96,7 @@ class Server:
         database_name = kwargs.get(Server.ARG_DATABASE_NAME_KEY, self.database_name)
         dbs = self.get_database_names()
         return database_name in dbs
+
 
     def create_db(self, **kwargs):
         if not self.is_connected:
@@ -79,6 +107,7 @@ class Server:
         print("Create MongoDb Server database: {}".format(database_name))
         return self.init_database(database=database_name)
 
+
     def drop_db(self, **kwargs):
         if not self.is_connected:
             print("Server must be connected to drop a database.")
@@ -87,6 +116,7 @@ class Server:
         print("Drop MongoDb Server database: {}".format(database_name))
         self.client.drop_database(database_name)
         return True
+
 
     def get_database_names(self):
         if not self.is_connected:
@@ -97,6 +127,7 @@ class Server:
         print("MongoDb Server database list: {}".format(dbs))
         return dbs
 
+
     def init_database(self, **kwargs):
         if not self.is_connected:
             print("Server must be connected to get database list.")
@@ -105,15 +136,36 @@ class Server:
         database_name = kwargs.get(Server.ARG_DATABASE_NAME_KEY, self.database_name)
         print("Initialize MongoDb Server database: {}".format(database_name))
 
-        database = self.client[database_name]
-        types_col = database["types"]
-
-        type_string = { "name": "String", "version": 1 }
-        x = types_col.insert_one(type_string)
-        print ("type_string inserted with id: {}".format(x.inserted_id))
-        type_number = { "name": "Number", "version": 1 }
-        x = types_col.insert_one(type_number)
-        print ("type_number inserted with id: {}".format(x.inserted_id))
+        self.add_type(Server.TYPE_STRING_VALUE, database=database_name)
+        self.add_type(Server.TYPE_NUMBER_VALUE, database=database_name)
 
         return True
 
+
+    def add_type(self, type_name, **kwargs):
+        if not self.is_connected:
+            print("Server must be connected to add a new type.")
+            return False
+
+        database_name = kwargs.get(Server.ARG_DATABASE_NAME_KEY, self.database_name)
+        print("Trying to add new type {} to database: {}...".format(type_name, database_name))
+
+        app_stack_type = dict()
+
+        app_stack_type[Server.TYPE_NAME_KEY] = type_name 
+
+        version = kwargs.get(Server.TYPE_VERSION_KEY, self.DEFAULT_TYPE_VERSION)
+        print("- {}: {}".format(Server.TYPE_VERSION_KEY, version))
+        app_stack_type[Server.TYPE_VERSION_KEY] = version
+
+        collection = kwargs.get(Server.TYPE_COLLECTION_KEY, self.DEFAULT_TYPE_COLLECTION)
+        print("- {}: {}".format(Server.TYPE_COLLECTION_KEY, collection))
+        app_stack_type[Server.TYPE_COLLECTION_KEY] = collection
+
+        database = self.client[database_name]
+        types_collection = database[Server.TYPES_COLLECTION_NAME]
+
+        result = types_collection.insert_one(app_stack_type)
+        print ("app_stack_type inserted with id: {}".format(result.inserted_id))
+
+        return result
